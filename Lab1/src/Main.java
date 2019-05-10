9da3f3b765b5f5cf8b0865fdf47c8a0907a302f3
Ernest Figura <209308@edu.p.lodz.pl>
2019-05-10 08:39:08 +0200

    Ernest print

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Grupa1 Operation Paxon96");
-        System.out.println("Poxx123");
+        System.out.println("Zandros229");
     }
 }
