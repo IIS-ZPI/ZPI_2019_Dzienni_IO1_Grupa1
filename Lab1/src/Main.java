db27b41e28f5dc030c8bf1c608e988266f2cce30
Zandros229 <ernest.figura548@gmail.com>
2019-05-15 15:16:56 +0200

    Add IArithmeticsDiv interface and implements it

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,9 +1,29 @@
-public class Main {
+public class Main implements IArithmeticsDiff, IArithmeticsMult, IArithmeticsAdd, IArithmeticsDiv {
 
     public static void main(String[] args) {
         System.out.println("Grupa1 Operation Paxon96");
         System.out.println("Zandros229");
         System.out.println("Poxx123");
         System.out.println("adamsylla97");
+    }
+
+    @Override
+    public double Difference(double A, double B) {
+        return A - B;
+    }
+
+    @Override
+    public double Multiplication(double A, double B) {
+        return A * B;
+    }
+
+    @Override
+    public double Addition(double A, double B) {
+        return A + B;
+    }
+
+    @Override
+    public double Divsion(double A, double B) {
+        return A / B;
     }
 }
