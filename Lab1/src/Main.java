d2e25a16cc6fce7ed83f1ee220c5b2140a43cb5d
Paxon96 <bwloczyk@gmail.com>
2019-05-15 15:02:53 +0200

    Add IArithmeticsDiff

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,14 +1,9 @@
-public class Main implements IArithmeticsMult{
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Grupa1 Operation Paxon96");
         System.out.println("Zandros229");
         System.out.println("Poxx123");
         System.out.println("adamsylla97");
-    }
-
-    @Override
-    public double Multiplication(double A, double B) {
-        return A*B;
     }
 }
