843273647d744919ac6309f6029b83895b7784e9
Adam Sylla <sylla.adam.97@gmail.com>
2019-05-15 14:47:41 +0200

    *implemented interface to Main method

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,7 +1,12 @@
-public class Main {
+public class Main implements IArithmeticsAdd{
 
     public static void main(String[] args) {
         System.out.println("Grupa1 Operation Paxon96");
         System.out.println("adamsylla97");
+    }
+
+    @Override
+    public double Addition(double A, double B) {
+        return A+B;
     }
 }
