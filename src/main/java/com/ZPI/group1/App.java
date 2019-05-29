7d895fb162915bcda9a8390725a143c3f80c7eac
Zandros229 <ernest.figura548@gmail.com>
2019-05-29 15:03:10 +0200

    Initialize Project

---
+++ src/main/java/com/ZPI/group1/App.java
@@ -0,0 +1,13 @@
+package com.ZPI.group1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ZPI/group1/ApiController/DataReciverTest.java
@@ -0,0 +1,9 @@
+package com.ZPI.group1.ApiController;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataReciverTest {
+
+}
