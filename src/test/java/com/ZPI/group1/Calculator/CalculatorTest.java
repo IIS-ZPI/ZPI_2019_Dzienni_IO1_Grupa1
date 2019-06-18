85912c9325ad8af4576c0e27bd180f826e64c6a9
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-18 21:45:24 +0200

    *added tests for calculator mean, dominant, coefficientOfVariation, median, deviation

--- src/test/java/com/ZPI/group1/Calculator/CalculatorTest.java
+++ src/test/java/com/ZPI/group1/Calculator/CalculatorTest.java
@@ -23,29 +23,35 @@ public class CalculatorTest {
 
         preparedRatesList = new ArrayList<>();
 
+        Rate rate = new Rate();
+        rate.mid = 14.0;
         Rate rate1 = new Rate();
         rate1.mid = 10.0;
         Rate rate2 = new Rate();
         rate2.mid = 15.0;
         Rate rate3 = new Rate();
         rate3.mid = 17.0;
+        Rate rate4 = new Rate();
+        rate4.mid = 14.0;
 
+        preparedRatesList.add(rate);
         preparedRatesList.add(rate1);
         preparedRatesList.add(rate2);
         preparedRatesList.add(rate3);
+        preparedRatesList.add(rate4);
 
     }
 
     @Test
-    public void medianTest(){
+    public void medianShouldReturnProperValueTest(){
 
-        final double EXPECTED_MEDIAN = 15.0;
+        final double EXPECTED_MEDIAN = 14.0;
         Assertions.assertEquals(EXPECTED_MEDIAN,calculator.median(preparedRatesList));
 
     }
 
     @Test
-    public void meanTest(){
+    public void meanShouldReturnProperValueTest(){
 
         final double EXPECTED_MEAN = 14;
         Assertions.assertEquals(EXPECTED_MEAN, calculator.mean(preparedRatesList));
@@ -53,9 +59,9 @@ public class CalculatorTest {
     }
 
     @Test
-    public void deviationTest(){
+    public void deviationShouldReturnProperValueTest(){
 
-        double expectedDeviation = 3.605551275464;
+        double expectedDeviation = 2.5495097567964;
         expectedDeviation = expectedDeviation * 10000.0;
         expectedDeviation = (double) Math.round(expectedDeviation);
         expectedDeviation = expectedDeviation / 10000.0;
@@ -66,6 +72,32 @@ public class CalculatorTest {
         actualDeviation = actualDeviation / 10000.0;
 
         Assertions.assertEquals(expectedDeviation,actualDeviation);
+
+    }
+
+    @Test
+    public void coefficientOfVariationShouldReturnProperValueTest(){
+
+        double expectedCofficientOfVariation = 2.5495097567964/14;
+        expectedCofficientOfVariation = expectedCofficientOfVariation * 10000.0;
+        expectedCofficientOfVariation = (double) Math.round(expectedCofficientOfVariation);
+        expectedCofficientOfVariation = expectedCofficientOfVariation / 10000.0;
+
+        double actualCofficientOfVariation = calculator.deviation(preparedRatesList) / calculator.mean(preparedRatesList);
+        actualCofficientOfVariation = actualCofficientOfVariation * 10000.0;
+        actualCofficientOfVariation = (double) Math.round(actualCofficientOfVariation);
+        actualCofficientOfVariation = actualCofficientOfVariation / 10000.0;
+
+        Assertions.assertEquals(expectedCofficientOfVariation, actualCofficientOfVariation);
+
+    }
+
+    @Test
+    public void dominantShouldReturnProperValueTest(){
+
+        final double EXPECTED_DOMINANT = 14.0;
+
+        Assertions.assertEquals(EXPECTED_DOMINANT, calculator.dominant(preparedRatesList));
 
     }
 
