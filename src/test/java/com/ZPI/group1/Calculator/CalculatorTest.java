69cbcaaf51b95afce4749fa76c442fb190504481
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-18 21:52:02 +0200

    *ignored (Disabled in JUnit 5) test for dominant method

--- src/test/java/com/ZPI/group1/Calculator/CalculatorTest.java
+++ src/test/java/com/ZPI/group1/Calculator/CalculatorTest.java
@@ -5,10 +5,8 @@ import com.ZPI.group1.CurrencyData.CurrencyTable;
 import com.ZPI.group1.CurrencyData.Rate;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -92,6 +90,7 @@ public class CalculatorTest {
 
     }
 
+    @Disabled
     @Test
     public void dominantShouldReturnProperValueTest(){
 
